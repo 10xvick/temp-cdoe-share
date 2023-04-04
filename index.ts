ccb5d0140add19f158e8427c5f3443ddfd291e25
10xvick <103027108+10xvick@users.noreply.github.com>
2023-04-04 14:30:41 +0530

    3 files modified, 2 added

--- index.ts
+++ index.ts
@@ -1,6 +1,24 @@
+
+
+
+console.log('xxxxx', GIFReader );
 // Import stylesheets
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+const appDiv: HTMLElement = document.getElementById('app'); 
+
+const canvas:HTMLCanvasElement = document.getElementById('can');
+const ctx:CanvasRenderingContext2D = canvas.getContext('2d');
+
+const img = new Image();
+img.src = "http://bestsexgif.com/wp-content/uploads/2015/03/dildo-riding-hot-babe.gif";
+img.onload = (e)=>{
+  const size = 100;
+  ctx.drawImage(img,0,0,size,size);
+}
+
+
+// console.log(ctx);
+
+
